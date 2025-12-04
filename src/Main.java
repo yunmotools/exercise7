556e078be71990edd61d8d16dd40a66a9b25da98
jordonjones <jordon01jones@gmail.com>
2025-12-04 08:39:48 -0600

    exercise 7

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-
+        byte b = 1;
+        short s = 2;
+        int i = 32123;
+        long l = 12332123;
+        float f = 2.0f;
+        double d = 3.00 ;
+        boolean bo = true ;
+        char c = 'c';
     }
 }
